cb285c21deeee137866905dc9c0b337c23bc149f
RNY <rennya090611@gmail.com>
2024-02-05 15:40:20 +0900

    dockerでphpの環境構築

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PHP_Practice
 PHP練習用リポジトリ
+
+# PHP_DOC
+
+- [PHP マニュアル](https://www.php.net/manual/ja/index.php)
---
+++ src/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header class="header">
+        <h1 class="header__logo">
+            PHP練習
+        </h1>
+    </header>
+    <main class="main">
+        <h2 class="main__title">php 練習用リンク</h2>
+        <ol class="order-list">
+            <li class="order-list__item intro">
+                <a class="intro__link" href="./practice/hello.php">hello.php</a>
+                <p class="intro__discription">phpでhello worldを表示するプログラムです</p>
+            </li>
+            <li class="order-list__item intro">
+                <a class="intro__link" href="./practice/hello.php">hello.php</a>
+                <p class="intro__discription">phpでhello worldを表示するプログラムです</p>
+            </li>
+            <li class="order-list__item intro">
+                <a class="intro__link" href="./practice/hello.php">hello.php</a>
+                <p class="intro__discription">phpでhello worldを表示するプログラムです</p>
+            </li>
+            <li class="order-list__item intro">
+                <a class="intro__link" href="./practice/hello.php">hello.php</a>
+                <p class="intro__discription">phpでhello worldを表示するプログラムです</p>
+            </li>
+            <li class="order-list__item intro">
+                <a class="intro__link" href="./practice/hello.php">hello.php</a>
+                <p class="intro__discription">phpでhello worldを表示するプログラムです</p>
+            </li>
+            <li class="order-list__item intro">
+                <a class="intro__link" href="./practice/hello.php">hello.php</a>
+                <p class="intro__discription">phpでhello worldを表示するプログラムです</p>
+            </li>
+        </ol>
+        <div class="refarence">
+            <a target="blank" href="https://www.youtube.com/watch?v=HNHjpmdPFNU">参考動画</a>
+        </div>
+    </main>
+    <footer class="footer">
+        <small class="footer__copyright">&copy; 2024 hisamatsu renya</small>
+    </footer>
+</body>
+</html>
